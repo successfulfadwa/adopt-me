3b8321ca2d6fcf515e3cec6aa540ed9034e9391c
successfulfadwa <fadwa.aljaoui@gmail.com>
2023-04-09 10:15:23 +0200

    add setup for project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { createRoot } from "react-dom/client";
+import Pet from "./Pet";
+
+// delete the Pet component
+
+const App = () => {
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Luna" animal="dog" breed="Havanese" />
+      <Pet name="Pepper" animal="bird" breed="Cockatiel" />
+      <Pet name="Doink" animal="cat" breed="Mix" />
+    </div>
+  );
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
