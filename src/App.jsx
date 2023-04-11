9fec615a1b43dcb97cff7447eb10763d29724ee7
successfulfadwa <fadwa.aljaoui@gmail.com>
2023-04-11 11:00:25 +0200

    init router link

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import { createRoot } from "react-dom/client";
 import SearchParams from "./SearchParams";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Details from "./Details";
 //import Pet from "./Pet";
-
 
 // delete the Pet component
 
 const App = () => {
   return (
-    <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />;
-    </div>
+    
+      <BrowserRouter>
+        <h1>Adopt Me!</h1>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </BrowserRouter>
+   
   );
 };
 
