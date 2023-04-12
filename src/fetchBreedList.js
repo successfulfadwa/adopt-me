6aa445f988aa85ca7e87b693a32f88c12fa754f7
successfulfadwa <fadwa.aljaoui@gmail.com>
2023-04-12 16:17:57 +0200

    feat: add config 3

--- src/fetchBreedList.js
+++ src/fetchBreedList.js
@@ -1,19 +1,10 @@
-
 async function fetchBreedList({ queryKey }) {
-	
-
 	const animal = queryKey[1];
+  
 	if (!animal) return [];
   
 	const res = await fetch(
-	  `http://pets-v2.dev-apis.com/breeds?animal=${animal}`, {
-		headers: [
-			{ key: "Access-Control-Allow-Credentials", value: "true" },
-			{ key: "Access-Control-Allow-Origin", value: "*" },
-			{ key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-			{ key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-		  ]
-	  }
+	  `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
 	);
   
 	if (!res.ok) {
--- src/fetchPet.js
+++ src/fetchPet.js
@@ -1,13 +1,6 @@
 const fetchPet = async ({ queryKey }) => {
 	const id = queryKey[1];
-	const apiRes = await fetch(`http://pets-v2.dev-apis.com/pets?id=${id}`, {
-		headers: [
-			{ key: "Access-Control-Allow-Credentials", value: "true" },
-			{ key: "Access-Control-Allow-Origin", value: "*" },
-			{ key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-			{ key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-		  ]
-	  });
+	const apiRes = await fetch(`http://pets-v2.dev-apis.com/pets?id=${id}`);
   
 	if (!apiRes.ok) {
 	  throw new Error(`details/${id} fetch not ok`);
