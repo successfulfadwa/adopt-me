9362c572ffa909a4a4d42409f295b4c7bdfbc9ce
successfulfadwa <fadwa.aljaoui@gmail.com>
2023-04-18 14:51:56 +0200

    add loading page 2check

--- src/LoadingPage.jsx
+++ src/LoadingPage.jsx
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const LoadingPage = () => {
+  const [showGradient, setShowGradient] = useState(false);
+
+  useEffect(() => {
+    // Show the gradient after a short delay
+    const timeoutId = setTimeout(() => {
+      setShowGradient(true);
+    }, 500);
+
+    // Clear the timeout if the component is unmounted or the delay changes
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, []);
+
+  const gradientStyle = {
+    backgroundColor: 'rgb(251,193,161)',
+    background: showGradient ? 'linear-gradient(180deg, rgba(251,193,161,1) 0%, rgba(255,122,47,0.22454919467787116) 100%)' : 'rgb(251,193,161)',
+    transition: 'background 2s ease-in-out',
+  };
+
   return (
-    <div className="loading-page" style={{ backgroundColor: '#FFA500' }}>
+    <div className="loading-page" style={gradientStyle}>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 100 100"
