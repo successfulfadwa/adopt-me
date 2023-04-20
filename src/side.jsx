5fc681b495d960959190d6558e0eeb6ff489369d
successfulfadwa <fadwa.aljaoui@gmail.com>
2023-04-20 18:26:21 +0200

    add mail

--- src/side.jsx
+++ src/side.jsx
@@ -40,9 +40,13 @@ const Side = () => {
             }}
           >
             <span style={{ position: "relative", color: "#f5773d" }}>
-            <a href="mailto:fadwa.aljaoui.tech@gmail.com" style={{ textDecoration: "none", color: "#f5773d" }}>
-    Adopt.Me@gmail.com
-  </a>              <style>
+              <a
+                href="mailto:fadwa.aljaoui.tech@gmail.com"
+                style={{ textDecoration: "none", color: "#f5773d" }}
+              >
+                Adopt.Me@gmail.com
+              </a>{" "}
+              <style>
                 {`
         span:hover {
           color: #D66712 !important;
